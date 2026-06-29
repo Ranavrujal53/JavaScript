2db6a05906cb90a9f903501f9c293f7b89689a14
Ranavrujal53 <csrana1213@gmail.com>
2026-06-29 21:24:44 +0530

    react

---
+++ React js/class/myfirstapp/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home';
+
+function App() {
+  return ( 
+      <>
+      <h1>welcom to react app</h1>
+      <Home />
+      </>
+      );
+}
+
+      export default App;
